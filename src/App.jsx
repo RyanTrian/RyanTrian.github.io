051764e8324b530b3f6bb01e4e0ac313ef7c3e9c
Minh Truong <mptruong49@gmail.com>
2023-04-01 20:55:39 -0600

    added 404 error page and a wild card route to catch all undefined route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Nav from "./components/Nav"
 import Home from './pages/home';
+import Error404 from './components/Error404';
 
 import './App.css'
 
@@ -15,6 +16,14 @@ export default function App() {
         <Route 
           path="/"
           element={<Home />}
+        />
+        <Route 
+          path="/resume"
+          element={"Hello, no resume here yet"}
+        />
+        <Route
+          path="*"
+          element={<Error404 />} 
         />
       </Routes>
     </Router>
