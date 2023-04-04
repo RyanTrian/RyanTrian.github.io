54e36c33453e3cd2b37d4b9695a0cd1a6e91032f
Minh Truong <mptruong49@gmail.com>
2023-04-04 00:29:58 -0600

    added more conditions to render a tag

--- src/components/TextComponents/Paragraph.jsx
+++ src/components/TextComponents/Paragraph.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 export default function Paragraph({content}) {
   return (
-    <div class="text-md sm:text-lg md:text-xl lg:text-2xl text-gray-700">
+    <div className="text-md sm:text-lg md:text-xl lg:text-2xl text-gray-700">
       {
-        content.map((para) => (
-          <p className='pt-4 leading-8'>{para}</p>
-        ))
+        content.map((para, idx) => {
+          if (para.includes("<a>")) {
+            const strSplit = para.split('<a>');
+            console.log("🚀 ~ file: paragraph.jsx:10 ~ content.map ~ strSplit:", strSplit)
+            const textBeforeLink = strSplit[0];
+            const link = strSplit[1];
+            const textAfterLink = strSplit[2];
+            return (
+              <p key={idx} className='pt-4 leading-8'>
+                {textBeforeLink}
+                <a href={link} target='blank' className='text-blue-500 hover:text-blue-700 '>{textAfterLink}</a>
+              </p>
+            )
+          } else {
+            return (<p key={idx} className='pt-4 leading-8'>{para}</p>)
+          }
+        })
       }
     </div>
   )
