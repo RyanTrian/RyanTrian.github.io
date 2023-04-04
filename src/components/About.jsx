a9e02ce5d22de216de20ffe0fbc83787612d1ef0
Minh Truong <mptruong49@gmail.com>
2023-04-04 00:30:13 -0600

    About component

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Container from './TextComponents/Container'
+import Image from './TextComponents/Image'
+import Title from './TextComponents/Title'
+import Heading from './TextComponents/Heading'
+import Paragraph from './TextComponents/paragraph'
+
+const { title, src, heading, content } = {
+  title: "Hi, I'm Minh",
+  src: "https://user-images.githubusercontent.com/82792300/228434187-e4c589ca-aa70-43a0-8bad-65498a2cd1e3.JPG",
+  heading: "My Contact Info",
+  content: [
+    "I recently earned a Full Stack Web Development Certificate from the University of Minnesota on February 2023.", 
+    "I graduated with a Bachelor of Science in Biology from the Colorado State University, Fort Collins on May 2020.", 
+    "I'm passionate about bringing ideas online, also, sharing and teaching new concept.",
+    "I love food, movies, gaming, and reading.",
+    "An update version of my resume can be found <a>/resume<a>here"
+  ]
+}
+
+const contact = [
+  "Find me on <a>https://github.com/RyanTrian<a>Github",
+  "Connect with me on <a>https://www.linkedin.com/in/minhtruong49/<a>LinkedIn",
+  "Email me at <a>mailto:mptruong49@gmail.com<a>mptruong49@gmail.com"
+]
+
+export default function About() {
+  return (
+    <Container>
+      <Image src={src} />
+      <div className='pt-6'>
+        <Title title={title} />
+        <Paragraph content={content} />
+        <Heading heading={heading} />
+        <Paragraph content={contact} />
+      </div>
+    </Container>
+  )
+}
