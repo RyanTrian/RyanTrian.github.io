b630efcb33e5393dda8a99cf9c8dd58a577f8022
Minh Truong <mptruong49@gmail.com>
2023-04-05 11:41:43 -0600

    Format

--- src/components/TextComponents/Heading.jsx
+++ src/components/TextComponents/Heading.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function Heading({heading}) {
+export default function Heading({ heading }) {
   return (
-    <h2 class="mt-4 py-2 border-b-2 border-gray-200 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-900 font-bold">{heading}</h2>
-  )
+    <h2 className="mt-4 py-2 border-b-2 border-gray-200 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-900 font-bold">
+      {heading}
+    </h2>
+  );
 }
