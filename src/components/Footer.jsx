916ac465f12177d069a18c7468f7fd2c1097c310
Minh Truong <mptruong49@gmail.com>
2023-04-05 11:45:04 -0600

    class to className, added a social link, then format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,91 +1,200 @@
-import { WaveDown } from "./Wave"
+import { WaveDown } from "./Wave";
+import monkeytype from "../assets/images/monkeytype.png";
 
 export default () => {
-
   const footerNavs = [
-      {
-          href: '/',
-          name: 'Home'
-      },
-      {
-          href: '/about',
-          name: 'About'
-      },
-      {
-          href: '/#projects',
-          name: 'Projects'
-      },
-      {
-          href: '/resume',
-          name: 'Resume'
-      },
-  ]
+    {
+      href: "/",
+      name: "Home",
+    },
+    {
+      href: "/about",
+      name: "About",
+    },
+    {
+      href: "/#projects",
+      name: "Projects",
+    },
+    {
+      href: "/resume",
+      name: "Resume",
+    },
+  ];
 
   return (
     <>
       <WaveDown />
       <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8">
-          <div className="max-w-lg sm:mx-auto sm:text-center">
-              <img src="https://user-images.githubusercontent.com/82792300/228377517-f4b636da-1c53-407d-8cc5-087db798752d.png" className="w-14 sm:mx-auto" />
-              <p className="leading-relaxed mt-2 text-[15px]">
-                Feel free to reach out if you want a to build something together, have a question, or just want to connect
-                <a aria-label="Email" href="mailto:mptruogn49@gmail.com" className="mx-2 text-blue-500 hover:text-blue-700" target="blank">mptruong49@gmail.com</a>
-              </p>
+        <div className="max-w-lg sm:mx-auto sm:text-center">
+          <img
+            src="https://user-images.githubusercontent.com/82792300/228377517-f4b636da-1c53-407d-8cc5-087db798752d.png"
+            className="w-14 sm:mx-auto"
+          />
+          <p className="leading-relaxed mt-2 text-[15px]">
+            Feel free to reach out if you want to build something together, have
+            a question, or just want to connect
+            <a
+              aria-label="Email"
+              href="mailto:mptruogn49@gmail.com"
+              className="mx-2 text-blue-500 hover:text-blue-700"
+              target="blank"
+            >
+              mptruong49@gmail.com
+            </a>
+          </p>
+        </div>
+        <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
+          {footerNavs.map((item, idx) => (
+            <li key={idx} className=" hover:text-gray-800">
+              <a href={item.href}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-8 items-center justify-between sm:flex">
+          <div className="mt-4 sm:mt-0">
+            &copy; 2023 Crafted by me using Tailwind UI.
           </div>
-          <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
-              {
-                  footerNavs.map((item, idx) => (
-                      <li className=" hover:text-gray-800">
-                          <a key={idx} href={item.href}>
-                              { item.name }
-                          </a>
-                      </li>
-                  ))
-              }
-          </ul>
-          <div className="mt-8 items-center justify-between sm:flex">
-              <div className="mt-4 sm:mt-0">
-                  &copy; 2023 Crafted by me using Tailwind UI.
-              </div>
-              <div className="mt-6 sm:mt-0">
-                  <ul className="flex items-center space-x-4">
-                    {/* Twitter */}
-                      <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-                          <a href="https://twitter.com/TruogMinh" target="blank">
-                              <svg className="svg-icon w-6 h-6 text-blue-400" viewBox="0 0 20 20">
-                                  <path fill="none" d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path>
-                              </svg>
-                          </a>
-                      </li>
-                    {/* Github */}
-                      <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-                          <a href="https://github.com/RyanTrian" target="blank">
-                          <svg className="w-6 h-6" width="64px" height="64px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>github [#142]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"> </path> </g> </g> </g> </g></svg>
-                          </a>
-                      </li>
-                    {/* Linkedin */}
-                      <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-                          <a href="https://www.linkedin.com/in/minhtruong49/" target="blank">
-                          <svg className="w-6 h-6" width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#0A66C2" d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"></path></g></svg>
-                          </a>
-                      </li>
-                    {/* Leetcode */}
-                      <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-                          <a href="https://leetcode.com/RyanTrian/" target="blank">
-                          <svg className="w-6 h-6" fill="#000000" width="64px" height="64px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21.469 23.907l-3.595 3.473c-0.624 0.625-1.484 0.885-2.432 0.885s-1.807-0.26-2.432-0.885l-5.776-5.812c-0.62-0.625-0.937-1.537-0.937-2.485 0-0.952 0.317-1.812 0.937-2.432l5.76-5.844c0.62-0.619 1.5-0.859 2.448-0.859s1.808 0.26 2.432 0.885l3.595 3.473c0.687 0.688 1.823 0.663 2.536-0.052 0.708-0.713 0.735-1.848 0.047-2.536l-3.473-3.511c-0.901-0.891-2.032-1.505-3.261-1.787l3.287-3.333c0.688-0.687 0.667-1.823-0.047-2.536s-1.849-0.735-2.536-0.052l-13.469 13.469c-1.307 1.312-1.989 3.113-1.989 5.113 0 1.996 0.683 3.86 1.989 5.168l5.797 5.812c1.307 1.307 3.115 1.937 5.115 1.937 1.995 0 3.801-0.683 5.109-1.989l3.479-3.521c0.688-0.683 0.661-1.817-0.052-2.531s-1.849-0.74-2.531-0.052zM27.749 17.349h-13.531c-0.932 0-1.692 0.801-1.692 1.791 0 0.991 0.76 1.797 1.692 1.797h13.531c0.933 0 1.693-0.807 1.693-1.797 0-0.989-0.76-1.791-1.693-1.791z"></path> </g></svg>
-                          </a>
-                      </li>
-                  </ul>
-              </div>
+          <div className="mt-6 sm:mt-0">
+            <ul className="flex items-center space-x-4">
+              {/* Twitter */}
+              <li className="w-10 h-10 border rounded-full flex items-center justify-center">
+                <a href="https://twitter.com/TruogMinh" target="blank">
+                  <svg
+                    className="svg-icon w-6 h-6 text-blue-400"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill="none"
+                      d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"
+                    ></path>
+                  </svg>
+                </a>
+              </li>
+              {/* Github */}
+              <li className="w-10 h-10 border rounded-full flex items-center justify-center">
+                <a href="https://github.com/RyanTrian" target="blank">
+                  <svg
+                    className="w-6 h-6"
+                    width="64px"
+                    height="64px"
+                    viewBox="0 0 20 20"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    fill="#000000"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <title>github [#142]</title>{" "}
+                      <desc>Created with Sketch.</desc> <defs> </defs>{" "}
+                      <g
+                        id="Page-1"
+                        stroke="none"
+                        stroke-width="1"
+                        fill="none"
+                        fill-rule="evenodd"
+                      >
+                        {" "}
+                        <g
+                          id="Dribbble-Light-Preview"
+                          transform="translate(-140.000000, -7559.000000)"
+                          fill="#000000"
+                        >
+                          {" "}
+                          <g
+                            id="icons"
+                            transform="translate(56.000000, 160.000000)"
+                          >
+                            {" "}
+                            <path
+                              d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+                              id="github-[#142]"
+                            >
+                              {" "}
+                            </path>{" "}
+                          </g>{" "}
+                        </g>{" "}
+                      </g>{" "}
+                    </g>
+                  </svg>
+                </a>
+              </li>
+              {/* Linkedin */}
+              <li className="w-10 h-10 border rounded-full flex items-center justify-center">
+                <a
+                  href="https://www.linkedin.com/in/minhtruong49/"
+                  target="blank"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    width="64px"
+                    height="64px"
+                    viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        fill="#0A66C2"
+                        d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"
+                      ></path>
+                    </g>
+                  </svg>
+                </a>
+              </li>
+              {/* Leetcode */}
+              <li className="w-10 h-10 border rounded-full flex items-center justify-center">
+                <a href="https://leetcode.com/RyanTrian/" target="blank">
+                  <svg
+                    className="w-6 h-6"
+                    fill="#000000"
+                    width="64px"
+                    height="64px"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <path d="M21.469 23.907l-3.595 3.473c-0.624 0.625-1.484 0.885-2.432 0.885s-1.807-0.26-2.432-0.885l-5.776-5.812c-0.62-0.625-0.937-1.537-0.937-2.485 0-0.952 0.317-1.812 0.937-2.432l5.76-5.844c0.62-0.619 1.5-0.859 2.448-0.859s1.808 0.26 2.432 0.885l3.595 3.473c0.687 0.688 1.823 0.663 2.536-0.052 0.708-0.713 0.735-1.848 0.047-2.536l-3.473-3.511c-0.901-0.891-2.032-1.505-3.261-1.787l3.287-3.333c0.688-0.687 0.667-1.823-0.047-2.536s-1.849-0.735-2.536-0.052l-13.469 13.469c-1.307 1.312-1.989 3.113-1.989 5.113 0 1.996 0.683 3.86 1.989 5.168l5.797 5.812c1.307 1.307 3.115 1.937 5.115 1.937 1.995 0 3.801-0.683 5.109-1.989l3.479-3.521c0.688-0.683 0.661-1.817-0.052-2.531s-1.849-0.74-2.531-0.052zM27.749 17.349h-13.531c-0.932 0-1.692 0.801-1.692 1.791 0 0.991 0.76 1.797 1.692 1.797h13.531c0.933 0 1.693-0.807 1.693-1.797 0-0.989-0.76-1.791-1.693-1.791z"></path>{" "}
+                    </g>
+                  </svg>
+                </a>
+              </li>
+              {/* MonkeyType */}
+              <li className="w-10 h-10 border rounded-full flex items-center justify-center">
+                <a href="https://monkeytype.com/profile/ryantruong" target="blank">
+                  <img src={monkeytype} alt="monkeytype" className="w-6" />
+                </a>
+              </li>
+            </ul>
           </div>
-          <style jsx>{`
-              .svg-icon path,
-              .svg-icon polygon,
-              .svg-icon rect {
-                  fill: currentColor;
-              }
-          `}</style>
+        </div>
+        <style jsx>{`
+          .svg-icon path,
+          .svg-icon polygon,
+          .svg-icon rect {
+            fill: currentColor;
+          }
+        `}</style>
       </footer>
     </>
-  )
-}
+  );
+};
