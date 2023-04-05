e312f1c76c466fb650f256a66f091dc8797df4f1
Minh Truong <mptruong49@gmail.com>
2023-04-05 11:42:41 -0600

    class to className then format

--- src/components/TextComponents/Container.jsx
+++ src/components/TextComponents/Container.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function Container({children}) {
+export default function Container({ children }) {
   return (
-    <div class="mt-3 bg-gray-50">
-            <div class="max-w-2xl px-10 py-6 mx-auto">
-              {children}
-            </div>
+    <div className="mt-3 bg-gray-50">
+      <div className="max-w-2xl px-10 py-6 mx-auto">{children}</div>
     </div>
-  )
+  );
 }
